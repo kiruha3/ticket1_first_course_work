6fe92e4d47cd6bd331b16b2f435de2f937b02409
kirill <kiruha3@mail.ru>
2023-07-12 16:26:40 +0700

    выполнено:)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] massiv = new int[]{1, 2, 3};
+        System.out.println("reverseMassive(massiv) = " + Arrays.toString((reverseMassive(massiv))));
     }
 
+    public static int[] reverseMassive(int[] massiv) {
+        int[] reverseMassive = new int[massiv.length];
+        for (int i = 0; i < massiv.length; i++) {
+            reverseMassive[i] = massiv[massiv.length - i - 1];
+        }
+        return reverseMassive;
+    }
 }
